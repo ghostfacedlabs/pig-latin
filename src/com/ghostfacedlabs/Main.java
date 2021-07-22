c28a62756d5b39e783ae98bf92f4d88598de6c61
ghostfacedlabs <ghostfacedlabs@gmail.com>
2021-07-22 05:46:42 -0700

    updated print format

--- src/com/ghostfacedlabs/Main.java
+++ src/com/ghostfacedlabs/Main.java
@@ -6,11 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a phrase to translate: ");
+        System.out.println("Enter a phrase to translate");
         String phrase = scanner.nextLine();
 
         String[] parts = phrase.split(" ");
-        System.out.println("Translation: ");
+        System.out.println("Translation");
         for (String part : parts) {
             System.out.print(translate(part) + " ");
         }
