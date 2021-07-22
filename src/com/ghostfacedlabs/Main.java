e764ed7998142c297627132389ee8a350e66ae22
ghostfacedlabs <ghostfacedlabs@gmail.com>
2021-07-22 05:39:51 -0700

    application v1.0 complete

--- src/com/ghostfacedlabs/Main.java
+++ src/com/ghostfacedlabs/Main.java
@@ -1,8 +1,23 @@
 package com.ghostfacedlabs;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a phrase to translate:");
+        String phrase = scanner.nextLine();
+
+        String[] parts = phrase.split(" ");
+
+        for (String part : parts) {
+            System.out.print(translate(part) + " ");
+        }
+
+    }
+
+    public static String translate(String s) {
+        return s.substring(1) + "-" + s.charAt(0) + "ay";
     }
 }
